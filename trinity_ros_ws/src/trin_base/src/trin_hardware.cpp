2390b958f7929a9028bbd420e1fa8bbf0ac9665a
gyawalisaurav <saurav.gyawali@yahoo.com>
2019-03-25 00:02:25 -0400

    Added error handling

--- trinity_ros_ws/src/trin_base/src/trin_hardware.cpp
+++ trinity_ros_ws/src/trin_base/src/trin_hardware.cpp
@@ -4,26 +4,34 @@ TrinHardware::TrinHardware(ros::NodeHandle nhi)
 {
     nh = nhi;
     arduino.setPort("/dev/ttyACM0");
-    arduino.open();
+    
+    try {
+        arduino.open();
+    } catch(serial::IOException &err) {
+        ROS_ERROR_STREAM("Connection failed with port: " << arduino.getPort());
+        ros::shutdown();
+    }
+
+    ROS_INFO("Connection established with Robot");
     arduino.setBaudrate(115200);
     serial::Timeout to = serial::Timeout::simpleTimeout(500);
     arduino.setTimeout(to);
 
-    hardware_interface::JointStateHandle left_wheel_joint("left_wheel", &pos[0], &vel[0], &eff[0]);
-    jnt_state_interface.registerHandle(left_wheel_joint);
+    hardware_interface::JointStateHandle left_joint("left_wheel", &pos[0], &vel[0], &eff[0]);
+    state_interface.registerHandle(left_joint);
 
-    hardware_interface::JointStateHandle right_wheel_joint("right_wheel", &pos[1], &vel[1], &eff[1]);
-    jnt_state_interface.registerHandle(right_wheel_joint);
+    hardware_interface::JointStateHandle right_joint("right_wheel", &pos[1], &vel[1], &eff[1]);
+    state_interface.registerHandle(right_joint);
 
-    registerInterface(&jnt_state_interface);
+    registerInterface(&state_interface);
 
-    hardware_interface::JointHandle left_wheel_vel(jnt_state_interface.getHandle("left_wheel"), &cmd[0]);
-    jnt_vel_interface.registerHandle(left_wheel_vel);
+    hardware_interface::JointHandle left_vel(state_interface.getHandle("left_wheel"), &cmd[0]);
+    vel_interface.registerHandle(left_vel);
 
-    hardware_interface::JointHandle right_wheel_vel(jnt_state_interface.getHandle("right_wheel"), &cmd[1]);
-    jnt_vel_interface.registerHandle(right_wheel_vel);
+    hardware_interface::JointHandle right_vel(state_interface.getHandle("right_wheel"), &cmd[1]);
+    vel_interface.registerHandle(right_vel);
 
-    registerInterface(&jnt_vel_interface);
+    registerInterface(&vel_interface);
 }
 
 void TrinHardware::readFromHardware()
@@ -39,12 +47,10 @@ void TrinHardware::readFromHardware()
     r = ((int32_t)buf[7] << 24) | ((int32_t)buf[6] << 16) | ((int32_t)buf[5] << 8) | ((int32_t)buf[4]);
     lv = ((int16_t)buf[9] << 8) | ((int16_t)buf[8]);
     rv = ((int16_t)buf[11] << 8) | ((int16_t)buf[10]);
-    pos[0] = (double)r * 0.006413900601;
-    pos[1] = (double)l * 0.006413900601;
+    pos[0] = (double)l * 0.006413900601;
+    pos[1] = (double)r * 0.006413900601;
     vel[0] = (double)lv / 1000;
     vel[1] = (double)rv / 1000;
-    // printf("%f %f %f %f\n",pos[0], pos[1], vel[0], vel[1]);
-    // printf("%f %f\n",pos[0], pos[1]);
     ReadMutex.unlock();
 }
 
@@ -53,13 +59,12 @@ void TrinHardware::writeToHardware()
     WriteMutex.lock();
     uint8_t temp[3];
     uint16_t left, right;
-    left = (cmd[0] * 10) + 400;
-    right = (cmd[1] * 10) + 400;
-    temp[0] = (0x03f8 & left) >> 3;
-    temp[1] = ((left & 0x7) << 5);
-    temp[1] |= (0x03e0 & right) >> 5;
-    temp[2] = ((right & 0x1f) << 3);
-    // printf("%f, %f\n", cmd[0], cmd[1]);
+    left = (cmd[0] * 10) + 1024;
+    right = (cmd[1] * 10) + 1024;
+    temp[0] = (0x07f0 & left) >> 4;
+    temp[1] = ((left & 0xf) << 4);
+    temp[1] |= (0x0780 & right) >> 7;
+    temp[2] = ((right & 0x7f) << 1);
     arduino.write(temp, 3);
     WriteMutex.unlock();
 }
